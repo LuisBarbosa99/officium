901df09556c99ed0dc23e53df70e56437cfe62a3
luisBarbosa99 <luis.barbosa@upe.br>
2021-09-07 14:15:09 -0300

    :sparkles: Implementa métodos de consulta de categorias

--- src/category/category.repository.ts
+++ src/category/category.repository.ts
@@ -1,7 +1,19 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, getConnection, Repository } from 'typeorm';
 import { Category } from './entities/category.entity';
 
 @EntityRepository(Category)
 export class CategoryRepository extends Repository<Category> {
+    
+    private finder = getConnection().createQueryBuilder().select("category").from(Category, "category");
 
+    async findOneByType(type: number): Promise<Category> {
+        return await this.finder
+            .where("category.type = :type")
+            .setParameters({type: type})
+            .getOneOrFail();
+    }
+
+    async findAll(): Promise<Category[]> {
+        return await this.finder.getMany();       
+    }
 }

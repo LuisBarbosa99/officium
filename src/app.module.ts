7ecbdecfefce746c36acef5a4531d8ac03f4cbed
luisBarbosa99 <luis.barbosa@upe.br>
2021-09-07 10:58:55 -0300

    fix: :hammer: refatora rotinas de login

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { AuthModule } from './auth/auth.module';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [AuthModule, TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
